50905fe8501866a8db0be1f89b9210cd14ff5bab
Pablo Mendez <mendez.developer@gmail.com>
2018-07-01 15:55:30 -0300

    added backend api

--- bookworm-react/src/components/forms/LoginForm.js
+++ bookworm-react/src/components/forms/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Form, Button } from 'semantic-ui-react'
+import { Form, Button, Message } from 'semantic-ui-react'
 import Validator from 'validator'
 import InlineError from '../messages/InlineError'
 
@@ -15,7 +15,7 @@ class LoginForm extends Component {
   }
 
   onChange = e => this.setState({
-    data: { ...this.state.data, [e.target.name]: e.target.value } 
+    data: { ...this.state.data, [e.target.name]: e.target.value }
   })
 
   onSubmit = () => {
@@ -23,15 +23,16 @@ class LoginForm extends Component {
     this.setState({ errors })
     if (Object.keys(errors).length === 0) {
       this.props.submit(this.state.data)
+        .catch(err => this.setState({ errors: err.response.data.errors }))
     }
   }
 
   validate = (data) => {
     const errors = {}
-    
+
     if (!Validator.isEmail(data.email)) errors.email = "Invalid email"
     if (!data.password) errors.password = "Can't be blank"
-    
+
     return errors
   }
 
@@ -39,13 +40,17 @@ class LoginForm extends Component {
     const { data, errors } = this.state
     return (
       <Form onSubmit={this.onSubmit}>
+        { errors.global && <Message negative>
+          <Message.Header>Something went wrong</Message.Header>
+          <p>{errors.global}</p>
+        </Message>}
         <Form.Field error={!!errors.email}>
           <label htmlFor="email">Email</label>
           <input type="email" id="email" name="email" placeholder="example@example.com"
             value={data.email}
             onChange={this.onChange}
           />
-          {errors.email && <InlineError text={errors.email}/>}
+          {errors.email && <InlineError text={errors.email} />}
         </Form.Field>
         <Form.Field error={!!errors.password}>
           <label htmlFor="password">Password</label>
@@ -53,7 +58,7 @@ class LoginForm extends Component {
             value={data.password}
             onChange={this.onChange}
           />
-          {errors.password && <InlineError text={errors.password}/>}
+          {errors.password && <InlineError text={errors.password} />}
         </Form.Field>
         <Button primary>Login</Button>
       </Form>
